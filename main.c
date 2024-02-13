1ea8cbeb79cf14ae130f3ad89cdcd883e9976815
jan_zima <zimcek1@gmail.com>
2024-02-13 11:26:23 +0100

    Blikání LED

--- main.c
+++ main.c
@@ -12,9 +12,20 @@ int main(void){
   stdio_init_all();
 
   gpio_init(PICO_DEFAULT_LED_PIN);
-  gpio_put(PICO_DEFAULT_LED_PIN, 1);
-  while(1){
-
-  }
-
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+  
+  while (true) {
+        for(uint16_t i = 0; i < 5; ++i){
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(250);
+        }
+        for(uint16_t i = 0; i < 8; ++i){
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(80);
+        }
+    }
 }
