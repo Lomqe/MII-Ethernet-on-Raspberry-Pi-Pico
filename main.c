3a2817e14dcb5de7e5d244d32241482bd94763c8
jan_zima <zimcek1@gmail.com>
2024-02-16 13:45:20 +0100

    Nastaveni 10 Mbps

--- main.c
+++ main.c
@@ -6,27 +6,28 @@
 #include "hardware/clocks.h"
 
 #include "defines.h"
+#include "miim.h"
 
 int main(void){
+  // De-assert reset
+  gpio_init(KSZ_RST);
+  gpio_set_dir(KSZ_RST, GPIO_OUT);
+  gpio_put(KSZ_RST, false);
 
-  // Reset signál KSZ na 0
-  gpio_init(RST_KSZ);
-  gpio_set_dir(RST_KSZ, GPIO_OUT);
-  gpio_put(RST_KSZ, false);
-
-  // Změna systém CLK na 100 MHZ
+  // Change sys_clk to 100 MHz
   set_sys_clock_khz(SYSTEM_FREQ_100000_KHZ, true);
   stdio_init_all();
-
-  gpio_init(PICO_DEFAULT_LED_PIN);
-  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
   uint32_t actual = clock_get_hz(clk_sys);
   printf("System clock is set to: %lu Hz\n", actual);
 
-  // TODO - nakonfigurovat 10 Mbps
+  // Set speed to 10 Mbps
+  mii_mdio_write(phy_address, 0, 0x0);
 
-  // Testovací program na blikání LED
+  // Blink test
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
   while (true) {
         for(uint16_t i = 0; i < 5; ++i){
           gpio_put(PICO_DEFAULT_LED_PIN, 1);
@@ -42,9 +43,10 @@ int main(void){
         }
 
         // test resetu KSZ
-        sleep_ms(15000);
-        gpio_put(RST_KSZ, true);
-        sleep_ms(10000);
-        gpio_put(RST_KSZ, false);
+        // sleep_ms(15000);
+        // gpio_put(KSZ_RST, true);
+        // sleep_ms(10000);
+        // gpio_put(KSZ_RST, false);
+        
     }
 }
