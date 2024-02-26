4dff78d9fd2ccce9704832ef61a7e7057b02b770
jan_zima <zimcek1@gmail.com>
2024-02-26 18:41:30 +0100

    Přidání NetCat packetu

--- defines.h
+++ defines.h
@@ -1,35 +1,5 @@
 #pragma once
 
-// The default PHY address is 00001 and may vary from 1-7 (0 = broadcast)
-uint8_t phy_address = 1u;
-
-// 67 BYTE LENGTH
-uint8_t test_packet[] = {
-    //  Ethernet II
-    0xC8, 0x7F, 0x54, 0xA5, 0x83, 0x5D,     // Destination MAC                        
-    0x58, 0x11, 0x22, 0x20, 0x74, 0x28,     // Source MAC
-    0x08, 0x00,                             // Type (IPv4)
-    //  IP
-    0x45, 0x00,                             // Header Length, Diff. Services Field
-    0x00, 0x35,                             // Total Length
-    0x02, 0xFD,                             // Identification
-    0x00, 0x00,                             // Fragment Offset
-    0x74,                                   // Time To Live
-    0x11,                                   // Protocol (UDP)
-    0x43, 0x6B,                             // Header checksum
-    0x5F, 0xA8, 0xDE, 0xCD,                 // Source Address
-    0xC0, 0xA8, 0x01, 0x32,                 // Destination Address
-    //  UDP
-    0x01, 0xBB,                             // Source Port
-    0xE8, 0x82,                             // Destination Port
-    0x00, 0x21,                             // Length
-    0x37, 0x7C,                             // Checksum
-    0x58, 0x0E, 0xF5, 0x70, 0xAC, 0x33, 0x47, 0x42, 0x69, 0x1B, 0xB7, 0x2E,         // DATA
-    0x91, 0xB8, 0x78, 0x8F, 0xA2, 0x67, 0x99, 0xF1, 0x32, 0xF1, 0x6A, 0xCF, 0x98    // DATA
-    };
-
-#define SYSTEM_FREQ_100000_KHZ 100000u
-
 #define KSZ_RST 2u
 #define KSZ_CRS_OR_CONFIG1 4u
 #define KSZ_COL_OR_CONFIG0 5u
@@ -49,5 +19,50 @@ uint8_t test_packet[] = {
 #define KSZ_RXD3_OR_PHYDA0 19u
 #define KSZ_MDC 20u
 #define KSZ_MDIO 21u
-#define KSZ_SENSOR1 26u
-#define KSZ_SENSOR2 27u
+
+#define RPP_SENSOR1 26u
+#define RPP_SENSOR2 27u
+
+#define SYSTEM_FREQ_100000_KHZ 100000u
+#define TX_FRAME_SIZE 1024u
+
+
+// The default PHY address is 00001 and may vary from 1-7 (0 = broadcast)
+uint8_t phy_address = 1u;
+
+uint8_t preamble[] = {0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0xD5};
+
+// 69 BYTE LENGTH
+#define NCAT_PACKET_LENGTH 69u
+uint8_t netcat_packet[] = {
+    //  Ethernet II
+    0xC8, 0x7F, 0x54, 0xA5, 0x83, 0x5D,     // Destination MAC                        
+    0xC8, 0x7F, 0x54, 0x69, 0x8A, 0xDA,     // Source MAC
+    0x08, 0x00,                             // Type (IPv4)
+    //  IP
+    0x45, 0x00,                             // Header Length, Diff. Services Field
+    0x00, 0x37,                             // Total Length
+    0xA2, 0xD8,                             // Identification
+    0x00, 0x00,                             // Fragment Offset
+    0x80,                                   // Time To Live
+    0x11,                                   // Protocol (UDP)
+    0x13, 0xD9,                             // Header checksum
+    0xC0, 0xA8, 0x01, 0x82,                 // Source Address
+    0xC0, 0xA8, 0x01, 0x32,                 // Destination Address
+    //  UDP
+    0xD5, 0x2A,                             // Source Port
+    0x04, 0xD2,                             // Destination Port
+    0x00, 0x23,                             // Length
+    0xD4, 0xF3,                             // Checksum
+    0x54, 0x65, 0x73, 0x74, 0x6F, 0x76, 0x61, 0x63, 0x69, 0x20, 0x7A, 0x70, 0x72,       // Data
+    0x61, 0x76, 0x61, 0x20, 0x7A, 0x20, 0x4E, 0x65, 0x74, 0x43, 0x61, 0x74, 0x0D, 0x0A  // Data
+    };
+
+uint8_t crc[4];
+static const uint32_t ethernet_polynom = 0xEDB88320U;
+
+static uint8_t tx_frame[TX_FRAME_SIZE];
+static uint8_t tx_frame_bits[TX_FRAME_SIZE * 2u];
+
+uint tx_dma;
+dma_channel_config tx_dma_config;
