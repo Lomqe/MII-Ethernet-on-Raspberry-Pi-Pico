d0ca03b101c4affe98ccd537adb45567e6f21f0d
jan_zima <zimcek1@gmail.com>
2024-02-26 18:42:42 +0100

    -

---
+++ includes.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// DEFAULT
+#include <stdio.h>
+
+// RPP
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/clocks.h"
+
+// PROJECT
+#include "defines.h"
+#include "miim.h"
+#include "test.h"
+#include "ethernet.h"
+
+// PIOASM
+#include "mii_tx.pio.h"
