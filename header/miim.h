6688f03bda86a46c5f524cbaaef9801772b641ff
jan_zima <zimcek1@gmail.com>
2024-04-23 17:30:51 +0200

    Deklarace funkcí rozhraní MIIM

---
+++ header/miim.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "pico/stdlib.h"
+
+static void mii_mdio_out(bool bit);
+static bool mii_mdio_in(void);
+void mii_mdio_write(uint addr, uint reg, int val);
+uint16_t mii_mdio_read(uint addr, uint reg);
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
