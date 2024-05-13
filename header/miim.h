6e11743fb8aec4abfb17600c0f2c1bc4bc2b635a
jan_zima <zimcek1@gmail.com>
2024-05-13 04:36:57 +0200

    Deklarace funkcí MIIM rozhraní

--- header/miim.h
+++ header/miim.h
@@ -5,6 +5,3 @@ static void mii_mdio_out(bool bit);
 static bool mii_mdio_in(void);
 void mii_mdio_write(uint addr, uint reg, int val);
 uint16_t mii_mdio_read(uint addr, uint reg);
-
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
