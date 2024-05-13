d9dc3bb7a9710239d5777933303a306b91589822
jan_zima <zimcek1@gmail.com>
2024-05-13 04:37:22 +0200

    Deklarace funkcí a LUT senzorů

--- header/sensors.h
+++ header/sensors.h
@@ -7,9 +7,9 @@ volatile static bool sampling = true;
 #define ADC_PIN_LEVEL 3.3   // ADC max value in Volts
 #define ADC_12B_LEVEL 4095  // ADC max 12 bit value
 
-static struct sensor{
+struct sensor{
     uint adc_input;         // Input to ADC 
-    uint8_t size;           // Size of the buffers
+    uint8_t size;           // Size of the buffer
     float sample_val;       // Last sampled valued by ADC
 
     uint8_t* lut_cm;        // Pointer to buffer with datasheet values
@@ -17,7 +17,7 @@ static struct sensor{
 
     float max_val;
     float min_val;
-}sensor1, sensor2;
+};
 
 #define SENSOR1_MAX_VAL sensor1_lut_volt[0]
 #define SENSOR1_MIN_VAL sensor1_lut_volt[17]
@@ -31,9 +31,11 @@ static uint8_t sensor1_lut_cm[] = {10, 12, 14, 16, 18, 20, 25, 30, 35, 40, 45, 5
 static float sensor2_lut_volt[] = {2.55, 1.99, 1.52, 1.25, 1.05, 0.90, 0.80, 0.73, 0.67, 0.60, 0.55, 0.50, 0.49, 0.48};
 static uint8_t sensor2_lut_cm[] = {20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150};
 
+
 ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
 
 
 uint8_t sensor_sample(struct sensor* s);
-void sensors_init();
+struct sensor sensors_init(uint8_t number);
+void write_sensor_data(uint8_t dist1, uint8_t dist2, uint32_t packet_count, uint8_t *packet);
