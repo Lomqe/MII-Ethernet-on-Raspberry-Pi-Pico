f204d875d1474f1500a849c3b7b31f02a5d974fb
jan_zima <zimcek1@gmail.com>
2024-05-13 04:36:31 +0200

    Deklarace ethernet funkcí

--- header/ethernet.h
+++ header/ethernet.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "pico/stdlib.h"
+#include "hardware/dma.h"
 
 // The default PHY address is 00001 and may vary from 1-7 (0 = broadcast)
-const static uint8_t phy_address = 1;
+static const uint8_t phy_address = 1;
 static const uint32_t ethernet_polynom = 0xEDB88320U;
 
 static uint ethernet_frame_crc2(const uint32_t *data, int length);
@@ -10,7 +11,3 @@ static uint ethernet_frame_crc(const uint8_t *data, int length);
 void mii_ethernet_output(uint8_t* tx_buffer, int length);
 void mii_ethernet_output_opt(uint8_t* tx_buffer, int length);
 void mii_ethernet_output_opt2(uint32_t* tx_buffer, int length);
-void write_sensor_data(uint8_t dist1, uint8_t dist2, uint32_t packet_count);
-
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
